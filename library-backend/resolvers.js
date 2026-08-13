e3765d67fe214572a063f3b5a17cf971c2733ac7
eliasollila <elias.ollila99@gmail.com>
2026-08-13 16:16:57 +0300

    8.14

--- library-backend/resolvers.js
+++ library-backend/resolvers.js
@@ -6,14 +6,23 @@ const resolvers = {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
     
-    allBooks: async () => {
+    allBooks: async (root, args) => {
+      if (args.genre) {
+        return Book.find({ genres: { $in: [args.genre] } })
+      }
+      
       return Book.find({})
     },
     
-    allAuthors: async () => {
-      return Author.find({})
+    allAuthors: async () => Author.find({})
+  },
+  
+  Book: {
+    author: async (root) => {
+      return Author.findById(root.author)
     }
   },
+  
   Mutation: {
     addBook: async (root, args) => {
       let author = await Author.findOne({ name: args.author })
@@ -24,9 +33,18 @@ const resolvers = {
       }
 
       const book = new Book({ ...args, author: author._id })
-      await book.save()
+      return book.save()
+    },
+    
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name })
+      
+      if (!author) {
+        return null
+      }
 
-      return book
+      author.born = args.setBornTo
+      return author.save()
     }
   }
 }
