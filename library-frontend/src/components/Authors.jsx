41157f7da495a313dee19a482ea4db9be7b0352d
eliasollila <elias.ollila99@gmail.com>
2026-08-13 14:12:28 +0300

    3.11

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
+import { useMutation } from '@apollo/client/react'
+import { EDIT_AUTHOR, ALL_AUTHORS } from '../queries'
+
 const Authors = (props) => {
+  
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
+
   if (!props.show) {
     return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+    editAuthor({
+      variables: {name, setBornTo: Number(born)}
+    })
+    
+    setName('')
+    setBorn('')
+  
   }
 
   return (
@@ -22,6 +44,18 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input value={name} onChange={({ target }) => setName(target.value)}/>
+        </div>
+        <div>
+          born
+          <input value={born} onChange={({ target }) => setBorn(target.value)}/>
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
